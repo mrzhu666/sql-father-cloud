045b1c0b51e75ad8fd86a9c06a04399c3151b341
mrzhu666 <1052712811@qq.com>
2023-10-11 21:54:18 +0800

    update

---
+++ docker-service.bat
@@ -0,0 +1,18 @@
+::重构容器并运行。方便调试测试容器
+set file=docker-compose.service.yml
+set service=gateway-module
+
+::模块单独重新打包
+::使用call原因 https://blog.csdn.net/weixin_40195422/article/details/125621814
+call mvn clean package -pl :%service% -am -amd -Dmaven.test.skip=true
+::重构容器
+docker-compose -f %file% build %service%
+::启动容器
+docker-compose -f %file% up -d %service%
+::查看日志
+::start开启新窗口，因为无法ctrl c中断
+::cmd /K 防止运行错误自动关闭窗口
+start cmd /K docker-compose -f %file% logs %service% -f
+
+::查看gateway日志输出
+::start cmd /K docker-compose -f docker-compose.service.yml logs gateway-module -f
