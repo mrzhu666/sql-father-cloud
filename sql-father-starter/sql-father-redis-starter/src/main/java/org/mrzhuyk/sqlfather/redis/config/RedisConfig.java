b128b3497363d3cc2e0f98dfc1f0a5269743d693
mrzhu666 <1052712811@qq.com>
2024-07-16 22:22:24 +0800

    feat: import Redisson to use distributed lock

--- sql-father-starter/sql-father-redis-starter/src/main/java/org/mrzhuyk/sqlfather/redis/config/RedisConfig.java
+++ sql-father-starter/sql-father-redis-starter/src/main/java/org/mrzhuyk/sqlfather/redis/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package org.mrzhuyk.sqlfather.redis.config;
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +13,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private Integer port;
+
+    ////@Value("${spring.redis.password}")
+    ////private String password;
+    //
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress("redis://" + host + ":" + port);
+            //.setPassword("123456");
+        return Redisson.create(config);
+    }
+
+    
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
